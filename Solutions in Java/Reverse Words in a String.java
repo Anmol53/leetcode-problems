922969c8f4d72a08c9d52fb467069f774b80b76e
Anmol Agrawal <anmol.ag53@gmail.com>
2020-07-15 15:04:06 +0530

    Add files via upload

---
+++ Solutions in Java/Reverse Words in a String.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        String strs[] = s.trim().split(" ");
+        String o = "";
+        for(int i = strs.length - 1; i >= 0; i--){
+            String x = strs[i];
+            if(x.length() > 0){
+                o = o + x + " ";
+            }
+        }
+        return o.trim();
+    }
+}
