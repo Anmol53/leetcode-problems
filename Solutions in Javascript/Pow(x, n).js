10b71647b16f1a566e008df35cb0d13b9b824362
Anmol Agrawal <anmol.ag53@gmail.com>
2020-07-16 14:14:01 +0530

    Create Pow(x, n).js

---
+++ Solutions in Javascript/Pow(x, n).js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(x == 0.0){
+        return 0;
+    }
+    if(x == 1.0){
+        return 1;
+    }
+    if(n == 0.0){
+        return 1;
+    }
+    if(n == 1.0){
+        return x;
+    }
+    if(n < 0.0){
+        return 1 / myPow(x, -n);
+    }
+    return (n % 2 == 0) ? myPow(x * x, Math.floor(n / 2)) : x * myPow(x * x, Math.floor(n / 2));
+};
