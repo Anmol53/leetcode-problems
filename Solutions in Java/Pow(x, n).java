cde801f7af709327a184858d21e3750d74c9fd15
Anmol Agrawal <anmol.ag53@gmail.com>
2020-07-16 14:07:52 +0530

    Create Pow(x, n).java

---
+++ Solutions in Java/Pow(x, n).java
@@ -0,0 +1,21 @@
+class Solution {
+    public double myPow(double x, long n) {
+        System.out.println(n);
+        if(x == 0.0){
+            return 0;
+        }
+        if(x == 1.0){
+            return 1;
+        }
+        if(n == 0.0){
+            return 1;
+        }
+        if(n == 1.0){
+            return x;
+        }
+        if(n < 0.0){
+            return 1 / myPow(x, -n);
+        }
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+}
